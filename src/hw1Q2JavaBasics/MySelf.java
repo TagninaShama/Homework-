8b95e5fd9b197c73e7e7d9952f5664396459eef8
TagninaShama <shamatagnina@gmail.com>
2021-10-27 22:38:08 -0400

    10272021 10:38pm

---
+++ src/hw1Q2JavaBasics/MySelf.java
@@ -0,0 +1,27 @@
+package hw1Q2JavaBasics;
+
+/*Projects names are generally written in UpperCamelCase
+ *  Camel case example for a project - CoreJava
+ *  Alternative example of Camel case, for a project - Core_java 
+ *  Package names are generally written in lowerCamelCase
+ *  Camel case example for a package - javaBasics
+ *  Alternative example of Camel case, for a package - java_basics
+ */
+
+public class MySelf {
+	/*
+	 * Class names are written in UpperCamelCase Class names are usually nouns or
+	 * noun phrases. Camel case example for a class - JavaIntro Alternative example
+	 * of Camel case, for a class - Java_intro
+	 */
+
+	// Main method cannot be declared it is either implemented or is initialized.
+	// Main method is static and is the entry point for java.
+
+	public static void main(String[] args) {
+
+		
+
+	}
+
+}
